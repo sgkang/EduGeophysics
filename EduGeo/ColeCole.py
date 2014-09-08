c9630fd6d30dd60b1dca07cd1118d862960238e0
SEOGI KANG <sgkang09@gmail.com>
2014-09-08 14:49:14 -0700

    Seismic Example

--- EduGeo/ColeCole.py
+++ EduGeo/ColeCole.py
@@ -14,6 +14,7 @@ def ColeColeFplot(siginf, eta, tau, c, fmin, fmax):
     wt, tbase, omega_int = setFrequency(time)
     sigmaColeF_temp = siginf-siginf*eta*(1./(1.+(1.-eta)*(1j*omega_int*tau)**c))
     sigmaColeT = transFiltImpulse(sigmaColeF_temp, wt, tbase, omega_int, time, tol=1e-12)
+
     fig, ax = plt.subplots(1,2, figsize = (16, 5))
     ax[0].semilogx(omega/2./np.pi, sigmaColeF.real, 'ko-')
     ax[0].semilogx(omega/2./np.pi, sigmaColeF.imag, 'ro-')
@@ -27,8 +28,8 @@ def ColeColeFplot(siginf, eta, tau, c, fmin, fmax):
     ax[1].grid(True)
     ax[1].set_xlabel("Time (s)", fontsize = 16)
     ax[1].set_ylabel("Time domain Cole-Cole (S/m)", fontsize = 16)
-    ax[0].set_title("$\sigma_{\infty}$= "+str(siginf)+"$, \eta = $"+str(eta)+"$, \
-                     \\tau = $"+str(tau)+"and c="+str(c), fontsize = 14)
+    ax[0].set_title("$\sigma_{\infty}$= "+("%5.1e")%(siginf)+",  $\eta = $"+("%5.1f")%(eta)+",  $\
+                       \\tau = $"+("%5.1e")%(tau)+"  and $c$="+("%5.1f")%(c), fontsize = 14)
 
 if __name__ == '__main__':
     ColeColeFplot(0.1, 0.1, 0.01, 0.5, -5, 5)
